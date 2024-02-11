f1b609cb45ee6fdb8323fd809f1e43e892b5f25e
Pradip Neupane <pradip.neupane@tuni.fi>
2024-02-12 01:21:55 +0200

    Update GameStateException.java

--- round5/wordgame/src/main/java/fi/tuni/prog3/wordgame/GameStateException.java
+++ round5/wordgame/src/main/java/fi/tuni/prog3/wordgame/GameStateException.java
@@ -1,6 +1,6 @@
 package fi.tuni.prog3.wordgame;
 
-public class GameStateException extends RuntimeException {
+public class GameStateException extends Exception {
     public GameStateException(String msg) {
         super(msg);
     }
