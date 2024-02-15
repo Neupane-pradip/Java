1e961dd561c754b8e1cf9b54725e61cb78f5c340
PradipNeupane <neupanepradip32@gmail.com>
2024-02-16 00:10:19 +0200

    Program is running smothly

---
+++ round6/shapes/src/main/java/fi/tuni/prog3/shapes/IShapeMetrics.java
@@ -0,0 +1,16 @@
+
+package fi.tuni.prog3.shapes;
+
+/**
+ *
+ * @author Pradip_Neuapne
+ */
+public interface IShapeMetrics {
+   double PI = 3.14159;
+
+    String name();
+    double area();
+    double circumference();
+} 
+
+
