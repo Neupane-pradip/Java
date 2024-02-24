9f5396f62d9ac95cbfc01fb18c8f6fb2e4bc434e
PradipNeupane <neupanepradip32@gmail.com>
2024-02-24 23:42:52 +0200

    lets see

--- round7/jsoncountries/src/main/java/fi/tuni/prog3/jsoncountries/Country.java
+++ round7/jsoncountries/src/main/java/fi/tuni/prog3/jsoncountries/Country.java
@@ -1,44 +1,36 @@
 package fi.tuni.prog3.jsoncountries;
 
-public class Country {
+public class Country implements Comparable<Country>{
 
+    // Private elements
     private final String name;
     private final double area;
     private final long population;
     private final double gdp;
 
-    // Constructor
-    public Country(String name, double area, long population, double gdp){
-        this.name = name;
-        this.area = area;
-        this.population = population;
-        this.gdp = gdp;
+    public Country(String name, double area, long population,double gdp){
+        this.name=name;
+        this.area=area;
+        this.population=population;
+        this.gdp=gdp;
+    }
+    public String getName(){return name;}
+    public double getArea(){return area;}
+    public long getPopulation(){return population;}
+    public double getGdp(){return gdp;}
+ 
+    @Override
+    public int compareTo(Country o) {
+        return this.name.compareTo(o.name);
     }
 
-    // Getters
-    public String getName() {
-        return name;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public long getPopulation() {
-        return population;
-    }
-
-    public double getGdp() {
-        return gdp;
-    }
-
-    // toString method
+    // Obvious toString method to give info in wanted format
     @Override
     public String toString(){
         return String.format("%s%n" +
                         "  Area: %.1f km2%n" +
                         "  Population: %d%n" +
                         "  GDP: %.1f (2015 USD)%n",
-                name, area, population, gdp);
-    }
+                name,area,population,gdp);
+    }  
 }
--- round7/jsoncountries/src/main/java/fi/tuni/prog3/jsoncountries/CountryData.java
+++ round7/jsoncountries/src/main/java/fi/tuni/prog3/jsoncountries/CountryData.java
@@ -1,7 +1,7 @@
 package fi.tuni.prog3.jsoncountries;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -11,77 +11,52 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class CountryData {
 
-    // Method to read data from JSON files and create a list of Country objects
-    public static List<Country> readFromJsons(String areaFile, String populationFile, String gdpFile) {
-        TreeMap<String, String> areaData;
-        TreeMap<String, String> populationData;
-        TreeMap<String, String> gdpData;
+    public static List<Country> readFromJsons(String areaFile,
+                                              String populationFile,
+                                              String gdpFile) {
+        // Read JSON files into TreeMap
+        TreeMap<String, Double> areaData = readJsonFile(areaFile);
+        TreeMap<String, Long> populationData = readJsonFile(populationFile);
+        TreeMap<String, Double> gdpData = readJsonFile(gdpFile);
 
-        try {
-            // Read data from JSON files
-            areaData = readJsonFile(areaFile);
-            populationData = readJsonFile(populationFile);
-            gdpData = readJsonFile(gdpFile);
-        } catch (IOException e) {
-            System.out.println("Error reading files");
-            return null;
-        }
-
+        // Combine data from all files
         List<Country> countries = new ArrayList<>();
-        
-        // Iterate over each country name in the area data
-        for (String countryName : areaData.keySet()) {
-            
-            // Check if population and GDP data exist for the country
+        for (Map.Entry<String, Double> entry : areaData.entrySet()) {
+            String countryName = entry.getKey();
             if (populationData.containsKey(countryName) && gdpData.containsKey(countryName)) {
-                
-                // Parse data and create Country object
-                double area = Double.parseDouble(areaData.get(countryName));
-                long population = Long.parseLong(populationData.get(countryName));
-                double gdp = Double.parseDouble(gdpData.get(countryName));
+                double area = entry.getValue();
+                long population = populationData.get(countryName);
+                double gdp = gdpData.get(countryName);
                 countries.add(new Country(countryName, area, population, gdp));
             }
         }
         return countries;
     }
 
-    // Method to read JSON file and extract relevant data
-    
-    private static TreeMap<String, String> readJsonFile(String filename) throws IOException {
-        TreeMap<String, String> countryDataPoint = new TreeMap<>();
-        Gson gson = new Gson();
-        
-        // Read JSON file using Gson library
+    private static <T> TreeMap<String, T> readJsonFile(String filename) {
+        TreeMap<String, T> dataMap = new TreeMap<>();
         try (FileReader fileReader = new FileReader(filename)) {
-            JsonElement jsonElement = JsonParser.parseReader(fileReader);
-            
-            // Extract relevant data from JSON structure
-            JsonArray recordArray = jsonElement.getAsJsonObject()
-                    .getAsJsonObject("Root")
-                    .getAsJsonObject("data")
-                    .getAsJsonArray("record");
-            for (JsonElement element : recordArray) {
-                JsonObject fieldObject = element.getAsJsonObject().getAsJsonArray("field").get(0).getAsJsonObject();
-                String countryName = fieldObject.get("value").getAsString();
-                String value = fieldObject.getAsJsonArray("attributes").get(0).getAsJsonObject().get("value").getAsString();
-                countryDataPoint.put(countryName, value);
+            JsonObject jsonObject = JsonParser.parseReader(fileReader).getAsJsonObject();
+            for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+                dataMap.put(entry.getKey(), (T) entry.getValue());
             }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + filename);
         }
-        return countryDataPoint;
+        return dataMap;
     }
 
-    // Method to write list of Country objects to a JSON file
-    
-    public static void writeToJson(List<Country> countries, String countryFile) throws IOException {
-        Gson gson = new Gson();
-        // Write list of Country objects to JSON file using Gson
-        
+    public static void writeToJson(List<Country> countries, String countryFile) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try (FileWriter fileWriter = new FileWriter(countryFile)) {
             gson.toJson(countries, fileWriter);
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + countryFile);
         }
     }
 }
